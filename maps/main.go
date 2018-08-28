8652e620afda4c74a853834db6be22e51e818675
Ayush Goyal <ayush987goyal@gmail.com>
2018-08-28 21:00:40 +0530

    iterating maps

--- maps/main.go
+++ maps/main.go
@@ -5,18 +5,17 @@ import (
 )
 
 func main() {
-	// var colors map[string]string
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf754",
+		"white": "#ffffff",
+	}
 
-	colors := make(map[string]string)
+	printMap(colors)
+}
 
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#4bf754",
-	// }
-
-	colors["white"] = "#ffffff"
-
-	delete(colors, "white")
-
-	fmt.Println(colors)
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println(color, hex)
+	}
 }
